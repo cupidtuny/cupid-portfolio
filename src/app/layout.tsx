1eb1b31bfac5fd130b4a8acc6ee3d98d516b1c64
cupidtuny <tiny.dota9999@gmail.com>
2024-08-03 03:24:00 +0800

    updated the commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+import "./globals.css";
 
 // export const metadata = {
 //   title: "iNoah",
@@ -28,13 +28,11 @@ import './globals.css'
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
-  )
+  );
 }
