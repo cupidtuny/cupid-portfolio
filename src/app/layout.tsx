2ae18aa337022470aab26b33fd8f8e03f5f0dd36
cupidtuny <tiny.dota9999@gmail.com>
2026-04-19 13:49:23 -0400

    Added GA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 
 // export const metadata = {
 //   title: "iNoah",
@@ -33,6 +34,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-5K5MNE4T7X"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-5K5MNE4T7X');
+        `}
+      </Script>
     </html>
   );
 }
